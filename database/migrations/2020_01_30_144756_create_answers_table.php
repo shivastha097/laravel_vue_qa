09023f3ebca69fe00677641537c571bb9da73a67
shivastha097 <shivastha097@gmail.com>
2020-02-03 08:42:11 +0545

    Design answers table and rename answers column to answers_count in the questions table

--- database/migrations/2020_01_30_144756_create_answers_table.php
+++ database/migrations/2020_01_30_144756_create_answers_table.php
@@ -15,6 +15,10 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('user_id');
+            $table->text('body');
+            $table->integer('votes_count')->default(0);
             $table->timestamps();
         });
     }
